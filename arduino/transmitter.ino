37cdfe9b68278fff7726cd30de101e76e629052f
Brett Wilkins <brett@brett.geek.nz>
2014-10-06 19:12:50 +1100

    Trimming down dis shit yo :metal:

--- arduino/transmitter.ino
+++ arduino/transmitter.ino
@@ -5,60 +5,34 @@
 // Get the RFM69 and SPIFlash library at: https://github.com/LowPowerLab/
 #include <RFM69.h>
 #include <SPI.h>
-#include <SPIFlash.h>
 
 #define NODEID        2    //unique for each node on same network
 #define NETWORKID     100  //the same on all nodes that talk to each other
 #define GATEWAYID     1
 //Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 #define FREQUENCY   RF69_433MHZ
-//#define FREQUENCY   RF69_868MHZ
-//#define FREQUENCY     RF69_915MHZ
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
-//#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
-#ifdef __AVR_ATmega1284P__
-  #define LED           15 // Moteino MEGAs have LEDs on D15
-  #define FLASH_SS      23 // and FLASH SS on D23
-#else
-  #define LED           9 // Moteinos have LEDs on D9
-  #define FLASH_SS      8 // and FLASH SS on D8
-#endif
+
+#define LED           9 // Moteinos have LEDs on D9
 
 #define SERIAL_BAUD   115200
 
 int TRANSMITPERIOD = 300; //transmit a packet to gateway so often (in ms)
-char payload[] = "BUGG IS CRAZY FOR REALZ YEAH BOY THUG LIFE";
+char payload[] = "123 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char buff[20];
 byte sendSize=0;
 boolean requestACK = false;
-SPIFlash flash(FLASH_SS, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 RFM69 radio;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW
-  radio.setHighPower(); //uncomment only for RFM69HW!
-#endif
   radio.encrypt(ENCRYPTKEY);
   char buff[50];
-  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  sprintf(buff, "\nTransmitting at %d Mhz...", 433);
   Serial.println(buff);
-
-  if (flash.initialize())
-  {
-    Serial.print("SPI Flash Init OK ... UniqueID (MAC): ");
-    flash.readUniqueId();
-    for (byte i=0;i<8;i++)
-    {
-      Serial.print(flash.UNIQUEID[i], HEX);
-      Serial.print(' ');
-    }
-    Serial.println();
-  }
-  else
-    Serial.println("SPI Flash Init FAIL! (is chip present?)");
+  
 }
 
 long lastPeriod = -1;
@@ -82,33 +56,6 @@ void loop() {
     //  radio.encrypt(KEY);
     //if (input == 'e') //e=disable encryption
     //  radio.encrypt(null);
-
-    if (input == 'd') //d=dump flash area
-    {
-      Serial.println("Flash content:");
-      uint16_t counter = 0;
-
-      Serial.print("0-256: ");
-      while(counter<=256){
-        Serial.print(flash.readByte(counter++), HEX);
-        Serial.print('.');
-      }
-      while(flash.busy());
-      Serial.println();
-    }
-    if (input == 'e')
-    {
-      Serial.print("Erasing Flash chip ... ");
-      flash.chipErase();
-      while(flash.busy());
-      Serial.println("DONE");
-    }
-    if (input == 'i')
-    {
-      Serial.print("DeviceID: ");
-      word jedecid = flash.readDeviceId();
-      Serial.println(jedecid, HEX);
-    }
   }
 
   //check for any received packets
@@ -117,7 +64,7 @@ void loop() {
     Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
     for (byte i = 0; i < radio.DATALEN; i++)
       Serial.print((char)radio.DATA[i]);
-    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+    // Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
 
     if (radio.ACKRequested())
     {
@@ -128,15 +75,6 @@ void loop() {
     Serial.println();
   }
 
-  //send FLASH id
-  if(sendSize==0)
-  {
-    sprintf(buff, "FLASH_MEM_ID:0x%X", flash.readDeviceId());
-    byte buffLen=strlen(buff);
-    radio.sendWithRetry(GATEWAYID, buff, buffLen);
-    delay(TRANSMITPERIOD);
-  }
-
   int currPeriod = millis()/TRANSMITPERIOD;
   if (currPeriod != lastPeriod)
   {
@@ -144,12 +82,16 @@ void loop() {
     Serial.print("Sending[");
     Serial.print(sendSize);
     Serial.print("]: ");
-    for(byte i = 0; i < sendSize; i++)
+    for(byte i = 0; i < sendSize; i++) {
       Serial.print((char)payload[i]);
+    }
 
-    if (radio.sendWithRetry(GATEWAYID, payload, sendSize))
-     Serial.print(" ok!");
-    else Serial.print(" nothing...");
+    if (radio.sendWithRetry(GATEWAYID, payload, sendSize)) {
+      Serial.print(" ok!");
+    }
+    else {
+      Serial.print(" nothing...");
+    }
 
     sendSize = (sendSize + 1) % 31;
     Serial.println();
